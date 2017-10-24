0a731a1b95270532bcf5fb0ee487dbe927f7b0a8
pennypangCODE <pennypang.t@gmail.com>
2017-10-25 07:49:37 +1100

    new animation

--- reveal_animation.js
+++ reveal_animation.js
@@ -2,212 +2,182 @@
 //Main Page
   sr.reveal('.navbar',{
     duration: 2000,
-    origin:'bottom',
-    mobile:true
+    origin:'bottom'
   });
   sr.reveal('.render',{
     duration: 1000,
     delay: 1000,
-    origin:'bottom',
-    mobile:true
+    origin:'bottom'
   });
 
 //Inspiration
   sr.reveal('.inspoHead',{
     duration: 1000,
     origin:'bottom',
-    viewFactor: 0.5,
-    mobile:true
+    viewFactor: 0.5
   });
   sr.reveal('.inspoHead',{
     duration: 1000,
     origin:'bottom',
-    viewFactor: 0.5,
-    mobile:true
+    viewFactor: 0.5
   });
   sr.reveal('.inspo1',{
     duration: 2000,
     origin:'left',
     distance: '300px',
-    viewFactor: 0.7,
-    mobile:true
+    viewFactor: 0.7
   });
   sr.reveal('.inspo2',{
     duration: 2000,
     origin:'top',
     distance: '300px',
-    viewFactor: 0.7,
-    mobile:true
+    viewFactor: 0.7
   });
   sr.reveal('.inspo3',{
     duration: 2000,
     origin:'right',
     distance: '300px',
-    viewFactor: 0.7,
-    mobile:true
+    viewFactor: 0.7
   });
   sr.reveal('.inspo4',{
-    duration: 2000,,
-    mobile:true
+    duration: 2000,
     origin:'left',
     distance: '300px',
-    viewFactor: 0.7,
-    mobile:true
+    viewFactor: 0.7
   });
   sr.reveal('.inspo5',{
     duration: 2000,
     distance: '300px',
     origin:'bottom',
-    viewFactor: 0.7,
-    mobile:true
+    viewFactor: 0.7
   });
   sr.reveal('.inspo6',{
     duration: 2000,
     distance: '300px',
     origin:'right',
-    viewFactor: 0.7,
-    mobile:true
+    viewFactor: 0.7
   });
 
   //Precedent
   sr.reveal('.preHead',{
     duration: 1000,
     origin:'bottom',
-    viewFactor: 0.5,
-    mobile:true
+    viewFactor: 0.5
   });
   sr.reveal('.zahaLeft',{
     duration: 1000,
     origin:'left',
     distance: '300px',
-    viewFactor: 0.7,
-    mobile:true
+    viewFactor: 0.7
   });
   sr.reveal('.zahaRight',{
     duration: 1000,
     origin:'right',
     distance:'300px',
-    viewFactor: 0.7,
-    mobile:true
+    viewFactor: 0.7
   });
 
   //Plan
   sr.reveal('.planHead',{
     duration: 1000,
     origin:'bottom',
-    viewFactor: 0.5,
-    mobile:true
+    viewFactor: 0.5
   });
   sr.reveal('.firstfl',{
     duration: 1000,
     origin:'left',
     distance:'300px',
-    viewFactor: 0.9,
-    mobile:true
+    viewFactor: 0.9
   });
   sr.reveal('.secondfl',{
     duration: 1000,
     origin:'right',
     distance:'300px',
-    viewFactor: 0.9,
-    mobile:true
+    viewFactor: 0.9
   });
   sr.reveal('.thirdfl',{
     duration: 1000,
     origin:'left',
     distance:'300px',
-    viewFactor: 0.9,
-    mobile:true
+    viewFactor: 0.9
   });
   sr.reveal('.fourthfl',{
     duration: 1000,
     origin:'right',
     distance:'300px',
-    viewFactor: 0.9,
-    mobile:true
+    viewFactor: 0.9
   });
 
   //Elevations
   sr.reveal('.elevationHead',{
     duration: 1000,
     origin:'bottom',
-    viewFactor: 0.5,
-    mobile:true
+    viewFactor: 0.5
   });
   sr.reveal('.butt1',{
     duration: 1000,
     delay: 1010,
     origin:'bottom',
     distance:'300px',
-    viewFactor: 0.5,
-    mobile:true
+    viewFactor: 0.5
   });
   sr.reveal('.butt2',{
     duration: 1000,
     delay:1020,
     origin:'bottom',
     distance:'300px',
-    viewFactor: 0.5,
-    mobile:true
+    viewFactor: 0.5
   });
   sr.reveal('.butt3',{
     duration: 1000,
     delay:1030,
     origin:'bottom',
     distance:'300px',
-    viewFactor: 0.5,
-    mobile:true
+    viewFactor: 0.5
   });
   sr.reveal('.butt4',{
     duration: 1000,
     delay:1040,
     origin:'bottom',
     distance:'300px',
-    viewFactor: 0.5,
-    mobile:true
+    viewFactor: 0.5
   });
 
   sr.reveal('.inner1',{
     duration: 1000,
     origin:'left',
     distance:'300px',
-    viewFactor: 0.6,
-    mobile:true
+    viewFactor: 0.6
   });
   sr.reveal('.inner2',{
     duration: 1000,
     origin:'right',
     distance:'300px',
-    viewFactor: 0.6,
-    mobile:true
+    viewFactor: 0.6
   });
   sr.reveal('.inner3',{
     duration: 1000,
     origin:'left',
     distance:'300px',
-    viewFactor: 0.6,
-    mobile:true
+    viewFactor: 0.6
   });
   sr.reveal('.inner4',{
     duration: 1000,
     origin:'right',
     distance:'300px',
-    viewFactor: 0.6,
-    mobile:true
+    viewFactor: 0.6
   });
 
   //Section
   sr.reveal('.secHead',{
     duration: 1000,
     origin:'bottom',
-    viewFactor: 0.5,
-    mobile:true
+    viewFactor: 0.5
   });
   sr.reveal('.sectionDisplay',{
     duration: 1000,
     delay:2000,
     origin:'bottom',
     distance:'300px',
-    viewFactor: 0.7,
-    mobile:true
+    viewFactor: 0.7
   });
